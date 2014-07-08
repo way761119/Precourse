50976e5b9f9c4e98919b344ccaa6cc508df4a585
Leo Chang <way761119@gmail.com>
2014-07-08 01:14:28 -0400

    basics 2-7

---
+++ basics/basics_two.rb
@@ -0,0 +1,41 @@
+puts 4936 / 1000
+puts 4936 % 1000 /100
+puts 4936 % 1000 % 100 /10
+puts 4936 % 1000 % 100 % 10
+
+puts "---------basics 3---------"
+
+movies = {jaws: 1975,
+          anchorman: 2004,
+          man_of_steel: 2013,
+          a_beautiful_mind: 2001,
+          the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+puts "----------basics 4----------"
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+puts "-----------basics 5----------"
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "-----------basics 6----------"
+
+puts 3.24 * 3.24
+puts 2.13 * 2.13
+puts 123.13 * 123.13
