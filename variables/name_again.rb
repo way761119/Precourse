afd8022da16f31217fde898d52516f57c1109dbf
Leo Chang <way761119@gmail.com>
2014-07-10 02:27:47 -0400

    Exercise

---
+++ variables/name_again.rb
@@ -0,0 +1,5 @@
+puts "What's your first name?"
+first_name = gets.chomp
+puts "Thank you. What's your last name?"
+last_name = gets.chomp
+puts "Allright. So your full name is " + first_name + " " + last_name
---
+++ variables/nameerroe.rb
@@ -0,0 +1,1 @@
+#The 'shoes' has not been defined in this program or it is outside the scope.
